ec1039a7b5b174e778bb736469022d52a430407a
Muktar <mdmuktarali100@gmail.com>
2017-04-11 17:51:43 +0600

    Courses Design Page

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.one').owlCarousel({
+
+    })
+
+    /*Sticky Js*/
+
+
+
+});
+
+/*Mix it up
+$('').mixItUp({
+    animation: {
+        effects: 'fade rotateZ(-180deg)',
+        duration: 700
+    },
+
+});
+
+/*Select Two For Register*/
+
+
+
+
+
